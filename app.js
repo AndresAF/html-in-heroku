dccd70b4c1fbdecb7a83cdd49aca42a7d5aad54e
DESKTOP-SU7N3I6\USUARIO <andresagfa@outlook.com>
2022-01-12 11:51:56 -0600

    segundo commit, se agrego js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const app = require("express")();
+
+const express = require("express")
+const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
 
 app.listen(PORT, () => {
     console.log("hola")
 })
+app.use(express.static(__dirname + "/public"));
