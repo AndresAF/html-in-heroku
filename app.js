28045bea7477cc1bb78e7a014f1c651311f5c4cc
DESKTOP-SU7N3I6\USUARIO <andresagfa@outlook.com>
2022-01-11 21:19:26 -0600

    yeah yo subiendo todo por q hacker

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = require("express")();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log("hola")
+})
